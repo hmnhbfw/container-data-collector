26c9d3e18e8498eb75fa7be50c18f94bb6effd22
Pavel Tsayukov <p.tsayukov@gmail.com>
2023-11-15 16:08:25 +0300

    Improved docstrings

--- src/container_data_collector/common_typevars.py
+++ src/container_data_collector/common_typevars.py
@@ -1,11 +1,11 @@
-"""Common TypeVarS for generic classes and functions.
+"""Common `TypeVar`s for generic classes and functions.
 
-Inner — a type for the inner container, in which elements are directly put.
+`Inner` — a type for the inner container, in which elements are directly put.
 
-Outer — a type for the outer container for collection and grouping elements.
-If the number of groups is greater than zero, the Outer is a mapping container
-(usually a plain dict), according to the keys which the outermost grouping
-occurs. Otherwise, the Outer is the Inner.
+`Outer` — a type for the outer container for collection and grouping elements.
+If the number of groups is greater than zero, the `Outer` is a `Mapping` container
+(usually a plain `dict`), according to the keys which the outermost grouping
+occurs. Otherwise, the `Outer` is the `Inner`.
 """
 
 from typing import TypeVar
